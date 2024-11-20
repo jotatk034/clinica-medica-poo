71f4d69fe5a4ae0176f26f258feeabdf06a14de0
Rogelio <rogelio.claro@gmail.com>
2024-11-20 15:53:41 -0300

    Restante das classes para a clínica Médica
    # Conflicts:
    #	src/main/java/br/edu/imepac/administrativo/entidades/Funcionario.java

---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Consulta.java
@@ -0,0 +1,52 @@
+package br.edu.imepac.administrativo.entidades;
+
+import java.time.LocalDateTime;
+
+public class Consulta {
+    private int id;
+    private LocalDateTime dataHorario;
+    private String sintoma;
+    private String diagnostico;
+    private boolean estaAtiva;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHorario() {
+        return dataHorario;
+    }
+
+    public void setDataHorario(LocalDateTime dataHorario) {
+        this.dataHorario = dataHorario;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public boolean isEstaAtiva() {
+        return estaAtiva;
+    }
+
+    public void setEstaAtiva(boolean estaAtiva) {
+        this.estaAtiva = estaAtiva;
+    }
+}
---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Prontuario.java
@@ -0,0 +1,33 @@
+package br.edu.imepac.administrativo.entidades;
+
+public class Prontuario {
+    private int id;
+    private String receituario;
+    private String observacoes;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReceituario() {
+        return receituario;
+    }
+
+    public void setReceituario(String receituario) {
+        this.receituario = receituario;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+}
+
