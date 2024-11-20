ec6d5c8d0759adaae7046ba4add31ed876bd442d
Rogelio <rogelio.claro@gmail.com>
2024-11-20 00:29:59 -0300

    Salvando as alterações

---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Perfil.java
@@ -0,0 +1,294 @@
+package br.edu.imepac.administrativo.entidades;
+
+public class Perfil {
+    private int id;
+    private String nome;
+    private boolean cadastrarFuncionario;
+    private boolean lerFuncionario;
+    private boolean atualizarFuncionario;
+    private boolean deletarFuncionario;
+    private boolean listarFuncionario;
+    private boolean cadastrarPaciente;
+    private boolean lerPaciente;
+    private boolean atualizarPaciente;
+    private boolean deletarPaciente;
+    private boolean listarPaciente;
+    private boolean cadastrarConsulta;
+    private boolean lerConsulta;
+    private boolean atualizarConsulta;
+    private boolean deletarConsulta;
+    private boolean listarConsulta;
+    private boolean cadastrarEspecialidade;
+    private boolean lerEspecialidade;
+    private boolean atualizarEspecialidade;
+    private boolean deletarEspecialidade;
+    private boolean listarEspecialidade;
+    private boolean cadastrarConvenio;
+    private boolean lerConvenio;
+    private boolean atualizarConvenio;
+    private boolean deletarConvenio;
+    private boolean listarConvenio;
+    private boolean cadastrarProntuario;
+    private boolean lerProntuario;
+    private boolean atualizarProntuario;
+    private boolean deletarProntuario;
+    private boolean listarProntuario;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public boolean isCadastrarFuncionario() {
+        return cadastrarFuncionario;
+    }
+
+    public void setCadastrarFuncionario(boolean cadastrarFuncionario) {
+        this.cadastrarFuncionario = cadastrarFuncionario;
+    }
+
+    public boolean isLerFuncionario() {
+        return lerFuncionario;
+    }
+
+    public void setLerFuncionario(boolean lerFuncionario) {
+        this.lerFuncionario = lerFuncionario;
+    }
+
+    public boolean isAtualizarFuncionario() {
+        return atualizarFuncionario;
+    }
+
+    public void setAtualizarFuncionario(boolean atualizarFuncionario) {
+        this.atualizarFuncionario = atualizarFuncionario;
+    }
+
+    public boolean isDeletarFuncionario() {
+        return deletarFuncionario;
+    }
+
+    public void setDeletarFuncionario(boolean deletarFuncionario) {
+        this.deletarFuncionario = deletarFuncionario;
+    }
+
+    public boolean isListarFuncionario() {
+        return listarFuncionario;
+    }
+
+    public void setListarFuncionario(boolean listarFuncionario) {
+        this.listarFuncionario = listarFuncionario;
+    }
+
+    public boolean isCadastrarPaciente() {
+        return cadastrarPaciente;
+    }
+
+    public void setCadastrarPaciente(boolean cadastrarPaciente) {
+        this.cadastrarPaciente = cadastrarPaciente;
+    }
+
+    public boolean isLerPaciente() {
+        return lerPaciente;
+    }
+
+    public void setLerPaciente(boolean lerPaciente) {
+        this.lerPaciente = lerPaciente;
+    }
+
+    public boolean isAtualizarPaciente() {
+        return atualizarPaciente;
+    }
+
+    public void setAtualizarPaciente(boolean atualizarPaciente) {
+        this.atualizarPaciente = atualizarPaciente;
+    }
+
+    public boolean isDeletarPaciente() {
+        return deletarPaciente;
+    }
+
+    public void setDeletarPaciente(boolean deletarPaciente) {
+        this.deletarPaciente = deletarPaciente;
+    }
+
+    public boolean isListarPaciente() {
+        return listarPaciente;
+    }
+
+    public void setListarPaciente(boolean listarPaciente) {
+        this.listarPaciente = listarPaciente;
+    }
+
+    public boolean isCadastrarConsulta() {
+        return cadastrarConsulta;
+    }
+
+    public void setCadastrarConsulta(boolean cadastrarConsulta) {
+        this.cadastrarConsulta = cadastrarConsulta;
+    }
+
+    public boolean isLerConsulta() {
+        return lerConsulta;
+    }
+
+    public void setLerConsulta(boolean lerConsulta) {
+        this.lerConsulta = lerConsulta;
+    }
+
+    public boolean isAtualizarConsulta() {
+        return atualizarConsulta;
+    }
+
+    public void setAtualizarConsulta(boolean atualizarConsulta) {
+        this.atualizarConsulta = atualizarConsulta;
+    }
+
+    public boolean isDeletarConsulta() {
+        return deletarConsulta;
+    }
+
+    public void setDeletarConsulta(boolean deletarConsulta) {
+        this.deletarConsulta = deletarConsulta;
+    }
+
+
+    public boolean isListarConsulta() {
+        return listarConsulta;
+    }
+
+    public void setListarConsulta(boolean listarConsulta) {
+        this.listarConsulta = listarConsulta;
+    }
+
+
+    public boolean isCadastrarEspecialidade() {
+        return cadastrarEspecialidade;
+    }
+
+    public void setCadastrarEspecialidade(boolean cadastrarEspecialidade) {
+        this.cadastrarEspecialidade = cadastrarEspecialidade;
+    }
+
+
+    public boolean isLerEspecialidade() {
+        return lerEspecialidade;
+    }
+
+    public void setLerEspecialidade(boolean lerEspecialidade) {
+        this.lerEspecialidade = lerEspecialidade;
+    }
+
+    public boolean isAtualizarEspecialidade() {
+        return atualizarEspecialidade;
+    }
+
+    public void setAtualizarEspecialidade(boolean atualizarEspecialidade) {
+        this.atualizarEspecialidade = atualizarEspecialidade;
+    }
+
+
+    public boolean isDeletarEspecialidade() {
+        return deletarEspecialidade;
+    }
+
+    public void setDeletarEspecialidade(boolean deletarEspecialidade) {
+        this.deletarEspecialidade = deletarEspecialidade;
+    }
+
+    public boolean isListarEspecialidade() {
+        return listarEspecialidade;
+    }
+
+    public void setListarEspecialidade(boolean listarEspecialidade) {
+        this.listarEspecialidade = listarEspecialidade;
+    }
+
+    public boolean isCadastrarConvenio() {
+        return cadastrarConvenio;
+    }
+
+    public void setCadastrarConvenio(boolean cadastrarConvenio) {
+        this.cadastrarConvenio = cadastrarConvenio;
+    }
+
+    public boolean isLerConvenio() {
+        return lerConvenio;
+    }
+
+    public void setLerConvenio(boolean lerConvenio) {
+        this.lerConvenio = lerConvenio;
+    }
+
+    public boolean isAtualizarConvenio() {
+        return atualizarConvenio;
+    }
+
+    public void setAtualizarConvenio(boolean atualizarConvenio) {
+        this.atualizarConvenio = atualizarConvenio;
+    }
+
+    public boolean isDeletarConvenio() {
+        return deletarConvenio;
+    }
+
+    public void setDeletarConvenio(boolean deletarConvenio) {
+        this.deletarConvenio = deletarConvenio;
+    }
+
+    public boolean isListarConvenio() {
+        return listarConvenio;
+    }
+
+    public void setListarConvenio(boolean listarConvenio) {
+        this.listarConvenio = listarConvenio;
+    }
+
+    public boolean isCadastrarProntuario() {
+        return cadastrarProntuario;
+    }
+
+    public void setCadastrarProntuario(boolean cadastrarProntuario) {
+        this.cadastrarProntuario = cadastrarProntuario;
+    }
+
+    public boolean isLerProntuario() {
+        return lerProntuario;
+    }
+
+    public void setLerProntuario(boolean lerProntuario) {
+        this.lerProntuario = lerProntuario;
+    }
+
+    public boolean isAtualizarProntuario() {
+        return atualizarProntuario;
+    }
+
+    public void setAtualizarProntuario(boolean atualizarProntuario) {
+        this.atualizarProntuario = atualizarProntuario;
+    }
+
+    public boolean isDeletarProntuario() {
+        return deletarProntuario;
+    }
+
+    public void setDeletarProntuario(boolean deletarProntuario) {
+        this.deletarProntuario = deletarProntuario;
+    }
+
+    public boolean isListarProntuario() {
+        return listarProntuario;
+    }
+
+    public void setListarProntuario(boolean listarProntuario) {
+        this.listarProntuario = listarProntuario;
+    }
+}
